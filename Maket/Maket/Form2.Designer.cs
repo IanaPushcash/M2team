beed2b85f5c8f330903586223431697c6f63942c
Dumb <Dumb>
2016-05-11 00:09:58 +0300

    first version

---
+++ Maket/Maket/Form2.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Maket
+{
+     partial class Form2
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.button1 = new System.Windows.Forms.Button();
+               this.button2 = new System.Windows.Forms.Button();
+               this.button3 = new System.Windows.Forms.Button();
+               this.listBox1 = new System.Windows.Forms.ListBox();
+               this.SuspendLayout();
+               // 
+               // button1
+               // 
+               this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+               this.button1.Location = new System.Drawing.Point(768, 40);
+               this.button1.Name = "button1";
+               this.button1.Size = new System.Drawing.Size(128, 40);
+               this.button1.TabIndex = 1;
+               this.button1.Text = "Добавить";
+               this.button1.UseVisualStyleBackColor = false;
+               this.button1.Click += new System.EventHandler(this.button1_Click);
+               // 
+               // button2
+               // 
+               this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+               this.button2.Location = new System.Drawing.Point(768, 117);
+               this.button2.Name = "button2";
+               this.button2.Size = new System.Drawing.Size(128, 40);
+               this.button2.TabIndex = 2;
+               this.button2.Text = "Удалить";
+               this.button2.UseVisualStyleBackColor = false;
+               // 
+               // button3
+               // 
+               this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+               this.button3.Location = new System.Drawing.Point(743, 250);
+               this.button3.Name = "button3";
+               this.button3.Size = new System.Drawing.Size(185, 40);
+               this.button3.TabIndex = 3;
+               this.button3.Text = "Подобрать рецепт";
+               this.button3.UseVisualStyleBackColor = false;
+               this.button3.Click += new System.EventHandler(this.button3_Click);
+               // 
+               // listBox1
+               // 
+               this.listBox1.FormattingEnabled = true;
+               this.listBox1.Items.AddRange(new object[] {
+            "ОГУРЦЫ",
+            "ПАТЛАЖЕЛЕ",
+            "ЯЙЦЫ",
+            "МЯСО",
+            "",
+            "",
+            "",
+            "РЫБА",
+            "",
+            "",
+            "",
+            "СЕМГА"});
+               this.listBox1.Location = new System.Drawing.Point(438, 117);
+               this.listBox1.Name = "listBox1";
+               this.listBox1.Size = new System.Drawing.Size(226, 186);
+               this.listBox1.TabIndex = 4;
+               // 
+               // Form2
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.BackgroundImage = global::Maket.Properties.Resources.grt;
+               this.ClientSize = new System.Drawing.Size(1005, 450);
+               this.Controls.Add(this.listBox1);
+               this.Controls.Add(this.button3);
+               this.Controls.Add(this.button2);
+               this.Controls.Add(this.button1);
+               this.Name = "Form2";
+               this.Text = "Содержимое холодильника";
+               this.ResumeLayout(false);
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.Button button1;
+          private System.Windows.Forms.Button button2;
+          private System.Windows.Forms.Button button3;
+          private System.Windows.Forms.ListBox listBox1;
+     }
+}
---
+++ Maket/Maket/Form3.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Maket
+{
+     partial class Form3
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.label1 = new System.Windows.Forms.Label();
+               this.comboBox1 = new System.Windows.Forms.ComboBox();
+               this.comboBox2 = new System.Windows.Forms.ComboBox();
+               this.button1 = new System.Windows.Forms.Button();
+               this.SuspendLayout();
+               // 
+               // label1
+               // 
+               this.label1.AutoSize = true;
+               this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+               this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+               this.label1.Location = new System.Drawing.Point(0, 20);
+               this.label1.Name = "label1";
+               this.label1.Size = new System.Drawing.Size(331, 50);
+               this.label1.TabIndex = 3;
+               this.label1.Text = "Выберите категорию продуктов\r\n                и сам продукт";
+               // 
+               // comboBox1
+               // 
+               this.comboBox1.FormattingEnabled = true;
+               this.comboBox1.Items.AddRange(new object[] {
+            "Овощи",
+            "Фрукты",
+            "Мясо",
+            "Рыба",
+            "Грибы",
+            "Молочка",
+            "Напитки",
+            "Консервы",
+            "Другое"});
+               this.comboBox1.Location = new System.Drawing.Point(113, 100);
+               this.comboBox1.Name = "comboBox1";
+               this.comboBox1.Size = new System.Drawing.Size(121, 21);
+               this.comboBox1.TabIndex = 4;
+               this.comboBox1.Text = "Категория";
+               // 
+               // comboBox2
+               // 
+               this.comboBox2.FormattingEnabled = true;
+               this.comboBox2.Items.AddRange(new object[] {
+            "",
+            "$",
+            "$",
+            "$",
+            "$",
+            "$",
+            "$",
+            "$",
+            "$",
+            "$$"});
+               this.comboBox2.Location = new System.Drawing.Point(113, 157);
+               this.comboBox2.Name = "comboBox2";
+               this.comboBox2.Size = new System.Drawing.Size(121, 21);
+               this.comboBox2.TabIndex = 5;
+               this.comboBox2.Text = "Продукт";
+               // 
+               // button1
+               // 
+               this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+               this.button1.Location = new System.Drawing.Point(113, 228);
+               this.button1.Name = "button1";
+               this.button1.Size = new System.Drawing.Size(128, 40);
+               this.button1.TabIndex = 6;
+               this.button1.Text = "Добавить";
+               this.button1.UseVisualStyleBackColor = false;
+               this.button1.Click += new System.EventHandler(this.button1_Click);
+               // 
+               // Form3
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+               this.ClientSize = new System.Drawing.Size(343, 308);
+               this.Controls.Add(this.button1);
+               this.Controls.Add(this.comboBox2);
+               this.Controls.Add(this.comboBox1);
+               this.Controls.Add(this.label1);
+               this.Name = "Form3";
+               this.Text = "Добавление";
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.Label label1;
+          private System.Windows.Forms.ComboBox comboBox1;
+          private System.Windows.Forms.ComboBox comboBox2;
+          private System.Windows.Forms.Button button1;
+     }
+}
